c9b87ed0ce705b26990a06578e9ca9db5b35e4c7
Rafael Carreon <rafaelcarreon2k@gmail.com>
2026-06-29 20:32:30 +1200

    Added Identity, SQLite database and initial migration

--- LockedInWebApp/Program.cs
+++ LockedInWebApp/Program.cs
@@ -1,29 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using LockedInWebApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source=inventory.db"));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => {
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapStaticAssets();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
--- LockedInWebApp/obj/Debug/net9.0/LockedInWebApp.AssemblyInfo.cs
+++ LockedInWebApp/obj/Debug/net9.0/LockedInWebApp.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("LockedInWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b293488b2bd085bda84c360a3ebc7101b412cbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LockedInWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LockedInWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
